93f9f679ab6f66c39fa6e075544f0b04cb375769
shameran karim <shameran.karim@hotmail.com>
2024-11-13 13:46:44 +0100

    skapat startmeny

--- ParkeringsApp/Parkeringssystem.cs
+++ ParkeringsApp/Parkeringssystem.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ParkeringsApp
 {
@@ -10,11 +11,40 @@ namespace ParkeringsApp
         {
             while (true)
             {
-                Console.WriteLine("1. Parkera fordon");
-                Console.WriteLine("2. Lista fordon");
-                Console.WriteLine("3. Visa parkering");
-                Console.WriteLine("4. Checka ut fordon");
-                Console.WriteLine("5. Avsluta");
+                Console.Clear();  // Töm skärmen varje gång huvudmenyn visas
+                Console.WriteLine("Välj meny:");
+                Console.WriteLine("1. Kundmeny");
+                Console.WriteLine("2. Adminmeny");
+                Console.WriteLine("3. Avsluta");
+                Console.Write("Välj alternativ: ");
+                string val = Console.ReadLine();
+
+                switch (val)
+                {
+                    case "1":
+                        KundMeny();
+                        break;
+                    case "2":
+                        AdminMeny();
+                        break;
+                    case "3":
+                        return;
+                    default:
+                        Console.WriteLine("Ogiltigt val, försök igen.");
+                        break;
+                }
+            }
+        }
+
+        private void KundMeny()
+        {
+            while (true)
+            {
+                Console.Clear();  // Rensa skärmen när kundmenyn visas
+                Console.WriteLine("Kundmeny:");
+                Console.WriteLine("1. Check in");
+                Console.WriteLine("2. Check out");
+                Console.WriteLine("3. Tillbaka till huvudmenyn");
                 Console.Write("Välj alternativ: ");
                 string val = Console.ReadLine();
 
@@ -24,15 +54,9 @@ namespace ParkeringsApp
                         ParkeraFordon();
                         break;
                     case "2":
-                        parkeringshus.ListaFordons();
-                        break;
-                    case "3":
-                        parkeringshus.VisaParkering();  // Visa parkeringens status
-                        break;
-                    case "4":
                         CheckaUtFordon();
                         break;
-                    case "5":
+                    case "3":
                         return;
                     default:
                         Console.WriteLine("Ogiltigt val, försök igen.");
@@ -41,8 +65,55 @@ namespace ParkeringsApp
             }
         }
 
+        private void AdminMeny()
+        {
+            while (true)
+            {
+                Console.Clear();  // Rensa skärmen när adminmenyn visas
+                Console.WriteLine("Adminmeny:");
+                Console.WriteLine("1. Se parkering");
+                Console.WriteLine("2. Se/redigera parkeringstid");
+                Console.WriteLine("3. Se fordonsinformation");
+                Console.WriteLine("4. Modifiera parkeringsplats");
+                Console.WriteLine("5. Hantera tilläggsavgifter");
+                Console.WriteLine("6. Tillbaka till huvudmenyn");
+                Console.Write("Välj alternativ: ");
+                string val = Console.ReadLine();
+
+                switch (val)
+                {
+                    case "1":
+                        // Visa parkering med platsstatus
+                        parkeringshus.VisaParkering();
+                        break;
+                    case "2":
+                        // Kod för att se/redigera parkeringstid (är inte implementerat än)
+                        break;
+                    case "3":
+                        // Visa fordonsinformation för alla parkerade fordon
+                        parkeringshus.ListaFordons();  // Visa alla parkerade fordon
+                        break;
+                    case "4":
+                        // Kod för att modifiera parkeringsplats (inte implementerat)
+                        break;
+                    case "5":
+                        // Kod för att hantera tilläggsavgifter (inte implementerat)
+                        break;
+                    case "6":
+                        return;  // Gå tillbaka till huvudmenyn
+                    default:
+                        Console.WriteLine("Ogiltigt val, försök igen.");
+                        break;
+                }
+
+                // Lägg till en paus innan man återvänder till menyn
+                TillbakaTillMeny();
+            }
+        }
+
         private void ParkeraFordon()
         {
+            Console.Clear();  // Rensa skärmen innan parkering
             Console.Write("Ange registreringsnummer: ");
             string registreringsnummer = Console.ReadLine();
             Console.Write("Ange färg: ");
@@ -79,13 +150,22 @@ namespace ParkeringsApp
 
             string resultat = parkeringshus.ParkeraFordon(fordon, varaktighet);
             Console.WriteLine(resultat);
+            TillbakaTillMeny();
         }
 
         private void CheckaUtFordon()
         {
+            Console.Clear();  // Rensa skärmen innan check out
             Console.Write("Ange registreringsnummer för att checka ut: ");
             string registreringsnummer = Console.ReadLine();
             parkeringshus.CheckaUtFordon(registreringsnummer);
+            TillbakaTillMeny();
+        }
+
+        private void TillbakaTillMeny()
+        {
+            Console.WriteLine("Tryck på en tangent för att gå tillbaka till menyn.");
+            Console.ReadKey();
         }
     }
-}
+}   
